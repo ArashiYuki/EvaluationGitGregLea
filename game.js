e5754cfd550e1d91239ee266a5bc65abb6aab043
Mister-Axolotl <krlekraft@gmail.com>
2024-12-03 15:26:13 +0100

    ✨ Feature : function to create balls

--- game.js
+++ game.js
@@ -4,5 +4,19 @@ class Game {
         this.ctx = this.canvas.getContext('2d');
         this.canvas.width = 414;
         this.canvas.height = 896;
+
+    }
+    createBall() {
+        const x = Math.random() * (this.canvas.width - 40) + 20;
+        this.balls.push({
+            x: x,
+            y: -20,
+            radius: 15,
+            health: 3,
+            speed: 2,
+            vx: (Math.random() - 0.5) * 4
+        });
     }
 }
+
+window.onload = () => new Game();
