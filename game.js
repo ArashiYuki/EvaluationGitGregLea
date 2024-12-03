c20b0356b925b455f6cef8fa20ac43f81590ae32
MisterAxolotl <55125665+Mister-Axolotl@users.noreply.github.com>
2024-12-03 15:21:54 +0100

    ✨ Feature : Add class game

---
+++ game.js
@@ -0,0 +1,8 @@
+class Game {
+    constructor() {
+        this.canvas = document.getElementById('gameCanvas');
+        this.ctx = this.canvas.getContext('2d');
+        this.canvas.width = 414;
+        this.canvas.height = 896;
+    }
+}
